8432eb6710bdfb1f5246741277478de9a2fa98cc
projectraya <rminkova007@gmail.com>
2024-10-29 09:45:05 +0200

    search update

--- Filmtivity-Skeleton/scripts/search.js
+++ Filmtivity-Skeleton/scripts/search.js
@@ -1,3 +1,33 @@
+const API_KEY = '2663a47da530c754207396eef5db5fb1';
+
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('search-btn').addEventListener('click', function(){
+        const title = document.getElementById('search').value;
+        if(title.trim()){
+            fetchMovie(title);
+            document.getElementById('details-page').style.display = 'block'; //show details section
+
+        }else{
+            document.getElementById('details-page').style.display = 'none'; //hide details section
+        }
+    })
+})
+
+
 function fetchMovie(title) {
- 
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${title}`)
+
+        .then(response => response.json())
+        .then(data => {
+            const movie = data.results[0];
+            document.getElementById('title').textContent = movie.title;
+            document.getElementById('overview').textContent = movie.overview;
+            //document.getElementById('vote_average').textContent = movie.vote_average;
+            //document.getElementById('original_language').textContent = movie.original_language;
+            document.getElementById('poster').src = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
+        })
+        .catch(error => console.error(error))
 }
