613e2b2d7a7fa3c14dc0d0f921faaa986d33c530
projectraya <rminkova007@gmail.com>
2025-01-13 21:41:18 +0200

    update

---
+++ Filmtivity-Skeleton/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the "static" folder
+app.use(express.static(path.join(__dirname, 'static')));
+
+// Serve HTML files
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/all', (req, res) => {
+  res.sendFile(path.join(__dirname, 'all.html'));
+});
+
+app.get('/search', (req, res) => {
+  res.sendFile(path.join(__dirname, 'search.html'));
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+console.log("Hello, Filmtivity is running!");
